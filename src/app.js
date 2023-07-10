457b568d54940cbaad4b37c0d1455ded8acd5a3a
Eunbi <jenabill@naver.com>
2023-07-11 00:08:44 +0900

    add socket configuration

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const express = require('express');
 const cors = require('cors');
 
+
 // 환경 변수
 require("dotenv").config();
 
@@ -15,7 +16,39 @@ mongoose.connect(process.env.MONGO_DB_URL)
     console.log(err);
   });
 
+
+
 const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+io.on('connection', function(socket){
+
+  //로그인하면 이거 밑에 두개뜸
+  console.log('User Conncetion');
+  
+  socket.on('connect user', function(user){
+    console.log("Connected user ");
+    socket.join(user['roomName']);
+    console.log("roomName : ", user['roomName']);
+    console.log("state : ", socket.adapter.rooms);
+    io.emit('connect user', user);
+  });
+  
+  
+  //메세지 입력하면 서버 로그에 이거뜸
+  socket.on('chat message', function(msg){
+    console.log("Message " + msg['message']);
+    console.log("보내는 아이디 : ", msg['roomName']);
+    io.to(msg['roomName']).emit('chat message', msg);
+  });
+
+  socket.on('disconnect', function() { 
+    console.log('접속 종료') 
+  }) 
+});
+
+
 
 app.use(cors());
 app.use((err, req, res, next) => {
